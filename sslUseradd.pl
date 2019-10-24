910fecafdc030376bc20c7cdc3f32a1af14b61d7
caroldomokos <carol.domokos@gmail.com>
2019-10-24 12:18:14 +0200

    defined sub in use

--- sslUseradd.pl
+++ sslUseradd.pl
@@ -20,13 +20,30 @@ sub take_first # take the first key in the key_db
         return ($word);
 }
 
+sub in_use? 
+{
+	my $key_proposed = shift;
+	my @allocated = read_to_array ("alloc_db");#/# can move to var
+	my $in_use = 0;
+	foreach $key (@allocated) 
+	{
+		if ($key = $key_proposed)
+		{
+		my $in_use = 1;
+		printf ("Key is already in use!");
+		break;
+		}
+	}
+	return ($in_use);
+}
 #***********************
 sub run # main function*
 #***********************
 {
 say "Hello World";
-my @array = read_to_array ("key_db"); #/# can move to var
-take_first (@array);
+my @key_proposed = read_to_array ("key_db"); #/# can move to var
+my $keyProposed = take_first (@key_proposed);
+
 }
 # end function definitions 
 # ---------------------------------------------------------------------------
