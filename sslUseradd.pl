c3693484a94b5ee1c0686c5f850adf702b3099aa
caroldomokos <carol.domokos@gmail.com>
2019-10-24 09:44:22 +0200

    added some functions

--- sslUseradd.pl
+++ sslUseradd.pl
@@ -1,1 +1,31 @@
-#/usr/bin/env perl
+#!/usr/bin/env perl
+
+use warnings;
+use 5.18.0;
+
+#### function definitions
+
+sub read_to_array
+{
+my $file = shift;
+open (my $handle, '<', $file) or die ('Cannot open file: $!');
+chomp(my @lines = <$handle>);
+close $handle;
+return @lines
+}
+
+sub take_first # take the first key in the key_db
+{
+	my $word = shift;
+        say $word;
+}
+
+sub run # main function
+{
+say "Hello World";
+#my @array = ("first", "second", "third");
+my @array = read_to_array ("key_db");
+take_first (@array);
+}
+### entry point
+run ();
